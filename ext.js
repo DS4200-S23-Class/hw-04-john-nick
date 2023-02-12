ce93ab8192f0d61f8b2fbf69dad68a66ac262e82
jdrohan356 <jdrohan356@gmail.com>
2023-02-12 14:58:08 -0500

    Points added

---
+++ ext.js
@@ -0,0 +1,15 @@
+
+
+let starter_points = [(1, 2), (2, 4), (6, 2), (9, 9)]
+
+
+
+// Add comments to the functions for java script
+
+// function changecolor(){
+// 	let a = Math.random() * 256;
+// 	let b = Math.random() * 256;
+// 	let c = Math.random() * 256;
+// 	let x = "rgb("+a.toString()+","+b.toString()+","+c.toString()+")";
+// 	document.getElementById('shift').style.color = x;
+// }
